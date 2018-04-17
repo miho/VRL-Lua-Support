1bc61c9798d33639110c95209a0203f980fd9ff9
Michael Hoffer <info@michaelhoffer.de>
2018-04-17 17:06:53 +0200

    pore refactoring (auto discovery of .vrl folder seems to work)

---
+++ src/main/java/edu/gcsc/vrl/langsupport/ugluashell/LuaEditor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.gcsc.vrl.langsupport.ugluashell;
+
+import eu.mihosoft.vrl.annotation.ComponentInfo;
+import eu.mihosoft.vrl.annotation.OutputInfo;
+import eu.mihosoft.vrl.annotation.ParamInfo;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+@ComponentInfo(name="Lua Editor", category="VRL/Language/Lua")
+public class LuaEditor implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @OutputInfo(style = "silent", name = " ")
+    public String run(@ParamInfo(name=" ", style="ugluashell-code") String code) {
+        return code;
+    }
+    
+}
